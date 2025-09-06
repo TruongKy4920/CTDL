8de1b06db602d678bef3de8ce3c48793935865ef
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-09-06 14:46:10 +0700

    queue

--- format.cpp
+++ format.cpp
@@ -9,77 +9,58 @@ using namespace std;
 
 std::ifstream input("input.txt");
 std::ofstream output("output.txt");
+template <typename T>
 struct Node{
-    int data;
+    T data;
     Node* next;
     Node(){
         data=0;
         next=NULL;
     }
-    Node(int value){
-        data=value;
-        next=NULL;
-    }
+    Node(T value): data(value), next(NULL){}
 };
-struct Linked_list {
-    Node* Head;
+
+template <typename T>
+struct Queue {
+    Node<T> * Head;
+    Node<T> *Tail;
     int length;
-    Linked_list(){
-        Head=NULL;
-        length=0;
-    }
-    ~Linked_list(){}
+    Queue():Head(NULL),Tail(NULL),length(0){}
+    ~Queue(){}
+
     int& operator[](int index){
         int i=0;
-        for(Node* node=Head;node->next!=NULL;node=node->next){
+        for(Node<T>* node=Head;node->next!=NULL;node=node->next){
             if(i==index) return node->data;
             i++;
         }
       
     }
     void print(){
-        for(Node* node=Head;node!=NULL;node=node->next){
+        for(Node<T>* node=Head;node!=NULL;node=node->next){
            cout<<node->data<<" ";
         }
     }
     
-    void push_back(int value){
-        Node* new_node= new Node(value);
+    void enqueue(int value){
+        Node<T>* new_node= new Node(value);
         length++;
         if(Head==NULL){
             Head=new_node;
+            Tail=new_node;
             return;
         }
-        Node* node=Head;
-        while(node->next!=NULL) node=node->next;
-        node->next=new_node;
+       Tail->next=new_node;
+       Tail=new_node;
        
     }
-   void erase(int index){
-    length--;
-    if(index<0 || index>length){
-        cout<<"out of range";
-        return;
-    }
-    if(index==0){
-        Node* node= Head;
-        Head = Head->next;
-        delete node;
-    }
-    int i=0;
-    Node* node=Head->next;
-    Node* node2=Head;
-    for(;node!=NULL;){
-           if(i==index) {
-            node2->next=node->next;
-            delete node;
-          
-           }
-           i++;
-           node2=node;
-           node=node->next;
-           
-        }
+   T dequeue(int index){
+        length--;
+        Node<T> * temp_node=Head;
+        T result = temp_node->data;
+        Head=Head->next;
+        delete temp_node;
+        return result;
    }
         
     
@@ -88,14 +69,15 @@ struct Linked_list {
 
 int main(){
     
-    Linked_list a;
-    a.push_back(2);
-    a.push_back(4);
-    a.push_back(8);
-    a.push_back(0);
-    a.push_back(6);
+    Queue <int> a;
+    a.enqueue(2);
+    a.enqueue(4);
+    a.enqueue(8);
+    a.enqueue(0);
+    a.enqueue(6);
+     a.print();
       //a.del(a.length-1);
-    a.erase(2);
+    cout<<"dequeue "<<a.dequeue(2)<<"\n";
     a.print();
       
       
